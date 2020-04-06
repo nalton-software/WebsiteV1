cb62f32938ec969b03f1c5f623f524e9f5ce6dc2
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2020-04-06 16:59:34 +0930

    killed old file, made phps edit file permissions

--- upc/clearTxt.php
+++ upc/clearTxt.php
@@ -1,4 +1,5 @@
 <?php
 $txtFile = $_GET["txtFile"];
+chmod($txtFile, 0666); // allow everyone to read, everyone to write
 file_put_contents($txtFile, "");
 ?>
--- upc/editTxt.php
+++ upc/editTxt.php
@@ -1,6 +1,7 @@
 <?php
 $txtFile = $_GET["txtFile"];
 $data = $_GET["data"];
+chmod($txtFile, 0666); // allow everyone to read, everyone to write
 $content = file_get_contents($txtFile);
 $content .= $data;
 file_put_contents($txtFile, $content);
